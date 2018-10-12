6e880c03372045dd54e38c980d5ed9bbda74b0cf
mosy <479979298@qq.com>
2018-10-12 19:21:32 +0800

    add

---
+++ src/test/java/com/sample/core/test/AllureTest.java
@@ -0,0 +1,13 @@
+package com.sample.core.test;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class AllureTest {
+	@Test
+	public void test() {
+		assertEquals(1, 1);
+	}
+
+}
