4c09fa48de48145cbda104e93efe64578e2b6848
mosy <479979298@qq.com>
2019-09-06 11:10:20 +0800

    1

--- src/test/java/com/sample/core/test/AllureTest.java
+++ src/test/java/com/sample/core/test/AllureTest.java
@@ -4,10 +4,32 @@ import static org.testng.Assert.assertEquals;
 
 import org.testng.annotations.Test;
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Step;
+import io.qameta.allure.Story;
+@Feature("接口测试")
 public class AllureTest {
 	@Test
+	@Step("登录")
+	@Story("111")
 	public void test() {
 		assertEquals(1, 1);
 	}
 
+	@Test
+	@Step("注册")
+	public void test1() {
+		assertEquals(1, 1);
+	}
+
+	@Test
+	@Step("修改")
+	public void test2() {
+		assertEquals(1, 1);
+	}
+
+	@Test
+	public void test3() {
+		assertEquals(1, 1);
+	}
 }
